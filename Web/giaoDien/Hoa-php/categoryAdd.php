02ac9915ba51ca4fc82f9746ff6dc5ddb04d7c0a
VanNemui <van.na205045@sis.hust.edu.vn>
2023-04-26 11:18:50 +0700

    front-end

---
+++ Web/giaoDien/Hoa-php/categoryAdd.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+include "slide.php";
+
+?>
+
+
+<div class="admin-content-right">
+            <div class="admin-content-right-category">
+                <h1>Thêm danh mục</h1>
+                <form action="" method="POST">
+                    <input type="text" placeholder="Nhập tên danh mục">
+                    <button type="submit">Thêm</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    
+</body>
+
+</html>
